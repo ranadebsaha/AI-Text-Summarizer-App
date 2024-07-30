e6701b07c7939475340e2d0d2bd7f4c822d14471
ranadebsaha <ranadebsaha2002@gmail.com>
2024-07-30 12:31:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const summarizeText = require('./summarize.js');
+
+// Parses JSON bodies (as sent by API clients)
+app.use(express.json());
+
+// Serves static files from the 'public' directory
+app.use(express.static('public'));
+
+app.post('/summarize', (req, res) => {
+  const text = req.body.text_to_summarize;
+
+   // call your summarizeText function, passing in the text from the request
+    summarizeText(text) 
+      .then(response => {
+         res.send(response); // Send the summary text as a response to the client
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
